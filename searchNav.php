3e56c11f964d3e6e8c519e6e7b1d75d95f178715
briangrimson <leifgrimson@gmail.com>
2017-03-06 16:46:26 -0500

    new attempt

--- searchNav.php
+++ searchNav.php
@@ -3,9 +3,9 @@
 <form method=get action="index.php">
 <!-- filter by work -->
 
-  <label>Black and White </label> <input type=checkbox name='bw' checked> <br>
-  <label >Color</label> <input type=checkbox  name='color'  checked><br>
-  <label > Photo Transfer </label> <input type=checkbox name='photo'  checked> <br>
+   <input type=checkbox name='bw' checked> <label>Black and White </label><br>
+   <input type=checkbox  name='color'  checked> <label >Color</label><br>
+   <input type=checkbox name='photo'  checked> <label > Photo Transfer </label> <br>
 <!-- filter by year -->
 <label>Year</label>
   <select  name=year>
@@ -16,10 +16,5 @@
   <input type=submit class='btn btn-primary btn-lg' value='Filter'>
 </form>
 
-  <form method=get action='index.php'>
-  <h3>keyword</h3>
-  <input type=text name=keyword><br>
-  <label><i>e.g. drawing, forest</i></label><br>
-  <input type=submit class='btn btn-primary btn-lg' value='Filter'>
-</form>
+
 </div>
