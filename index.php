cad77297f3861b3a6ad7731122d8c4f6eb051d86
briangrimson <leifgrimson@gmail.com>
2017-03-06 20:11:56 -0500

    add bootstrap

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
   <meta name="description" content="The artwork of Brian Grimson">
-
-
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <link rel=stylesheet href=css/main.css>
 </head>
 <body>
@@ -21,19 +21,24 @@
 
 <form method=get action="index.php">
 <!-- filter by work -->
+
 <h3> Series </h3>
    <input type=checkbox name='bw' checked> <label for='bw'>Black and White </label><br>
    <input type=checkbox  name='color'  checked> <label for='color' >Color</label><br>
    <input type=checkbox name='photo'  checked> <label for='photo' > Photo Transfer </label> <br>
+
 <!-- filter by year -->
 
-<h3>Year</h3>
+<label for='year'>Year</label>
+
   <select  name=year>
     <option value=''>All </option>
     <option value=2017>2017 </option>
     <option value=2016>2016 </option>
   </select><br>
-  <input type=submit class='' value='Filter'>
+
+
+  <input type=submit class='btn btn-primary btn-md' value='Filter'>
 </form>
 <div class='info contact'>
 <h3>Contact</h3>
--- searchNav.php
+++ searchNav.php
@@ -1,20 +1,32 @@
+<script>
+
+function check_boxes(){
+  $('label.chk').on("click", function () {
+    alert('hello');
+   });
+
+};
+
+
+</script>
+
+
 <div class=searchNav>
 <!-- form for search method -->
 <form method=get action="index.php">
 <!-- filter by work -->
 
-   <input type=checkbox name='bw' checked> <label>Black and White </label><br>
-   <input type=checkbox  name='color'  checked> <label >Color</label><br>
-   <input type=checkbox name='photo'  checked> <label > Photo Transfer </label> <br>
+   <input type=checkbox  name='bw' checked> <label   for='bw'>Black and White </label><br>
+   <input type=checkbox   name='color'  checked> <label  for='color'>Color</label><br>
+   <input type=checkbox  name='photo'  checked> <label  for='photo'> Photo Transfer </label> <br>
 <!-- filter by year -->
-<label>Year</label>
+<label for='year'>Year</label>
   <select  name=year>
     <option value=''>All </option>
     <option value=2017>2017 </option>
     <option value=2016>2016 </option>
   </select><br>
-  <input type=submit class='btn btn-primary btn-lg' value='Filter'>
+  <input type=submit class='submit' value='Filter'>
 </form>
-
 
 </div>
